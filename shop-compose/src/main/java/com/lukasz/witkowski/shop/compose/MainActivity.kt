fc8b5baaf2436409e004e48ab548f3f893b7110e
Lukasz Witkowski <lukasz.witkowski000@gmail.com>
2025-04-28 12:12:05 +0200

    Generate module

---
+++ shop-compose/src/main/java/com/lukasz/witkowski/shop/compose/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.lukasz.witkowski.shop.compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.lukasz.witkowski.shop.compose.ui.theme.Sunflower_mgrTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Sunflower_mgrTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Sunflower_mgrTheme {
+        Greeting("Android")
+    }
+}
