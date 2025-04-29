9e14c22ebc91e8f3158c5a3e7e115861c3c76bce
Lukasz Witkowski <lukasz.witkowski000@gmail.com>
2025-04-29 14:20:18 +0200

    Align product card

--- shop-compose/src/main/java/com/lukasz/witkowski/shop/compose/ProductsList.kt
+++ shop-compose/src/main/java/com/lukasz/witkowski/shop/compose/ProductsList.kt
@@ -42,11 +42,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
 import com.bumptech.glide.integration.compose.placeholder
+import com.lukasz.witkowski.shop.compose.model.ALL_PRODUCTS
 import com.lukasz.witkowski.shop.compose.model.Product
 
 @Composable
@@ -165,15 +167,25 @@ fun ProductCard(
                 )
             }
             IconButton(
+                modifier = Modifier.size(32.dp),
                 onClick = { onProductClicked(product) }
             ) {
                 Icon(
-                    Icons.Filled.PlayArrow,
+                    painterResource(R.drawable.arrow_right),
                     contentDescription = stringResource(R.string.more_details),
                     tint = MaterialTheme.colorScheme.primary,
-                    modifier = Modifier.size(32.dp),
+                    modifier = Modifier.size(32.dp)
                 )
             }
         }
     }
+}
+
+@Preview
+@Composable
+fun ProductCardPreview() {
+    ProductCard(
+        product = ALL_PRODUCTS.first(),
+        onProductClicked = {}
+    )
 }
